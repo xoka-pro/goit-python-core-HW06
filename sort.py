bd21864dc6c85746b5d09c5e53df63a561fdf017
Oleh Ovchinnikov <xokzzz@gmail.com>
2022-10-21 11:24:11 +0300

    working with sort_folder() add archive_handler try to add deleting folders function

--- sort.py
+++ sort.py
@@ -4,6 +4,10 @@ import os
 import sys
 from pathlib import Path
 
+
+found_folders = []
+
+
 def init(folder: Path) -> None:
     """ функція створює цільові директорії"""
 
@@ -52,13 +56,31 @@ def read_folder(path: Path) -> None:
 
     folders = ['images', 'documents', 'audio', 'video', 'archives', ]
 
+
     for element in path.iterdir():
         if element.is_dir() and element not in folders:
             read_folder(element)
         else:
-            sort_folder(el)
+            sort_folder(element)
+            found_folders.append(element)
 
     return None
+
+
+def archive_handler(file: Path) -> None:
+
+    folder_for_file = Path(normalize(file.name.replace(file.suffix, '')))
+    folder_for_file.mkdir(exist_ok=True, parents=True)
+
+    try:
+        shutil.unpack_archive(str(file.resolve()),
+                              str(folder_for_file.resolve()))
+    except shutil.ReadError:
+        print(f'Це не архів {file}!')
+        folder_for_file.rmdir()
+
+    return None
+
 
 def sort_folder(folder: Path) -> list:
     """
@@ -112,7 +134,9 @@ def sort_folder(folder: Path) -> list:
 
         if file.suffix[1:].upper() in archive_ext:
             archive_folder = Path('archives')
-            file.replace(archive_folder / normalize(file.name))
+            new_archive = archive_folder / normalize(file.name)
+            file.replace(new_archive)
+            archive_handler(new_archive)
             known_ext.append(file.suffix)
             archive_files.append(file.name)
 
@@ -120,11 +144,19 @@ def sort_folder(folder: Path) -> list:
             normalize(file.name)
             unknown_ext.append(file.suffix)
 
+
+        # try:
+        #     folder.rmdir()
+        # except OSError:
+        #     print(f'Помилка видалення папки {folder}')
+
+
     return known_ext, unknown_ext
 
 
 if __name__ == '__main__':
-    output_folder = Path('.')
+    output_folder = Path('test')
     print(normalize('ТеСтОвИй ТЕКСТ іїє 12345 <>@#$"".jpg'))
     init(output_folder)
     sort_folder(output_folder)
+    print(found_folders)
